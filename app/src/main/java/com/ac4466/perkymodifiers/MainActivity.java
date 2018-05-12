94a4db17b13c3f6d9d385097c75325c189622081
ac4466 <ac4466@gmail.com>
2018-05-12 10:41:55 -0700

    Initial commit

---
+++ app/src/main/java/com/ac4466/perkymodifiers/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ac4466.perkymodifiers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
